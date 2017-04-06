2272cd67dbf6c8294a0821f6b7b0c43f937a9995
Romain Brenguier <romain.brenguier@gmail.com>
2017-04-06 15:25:39 +0100

    [string-refine] Change the concretization process to avoid calling the solver diffblue/test-gen#158
    We do this by looking at the indexes for which we have concrete value
    because they are in the index set and assigning to the others the
    value of the next concrete value.
    This is more efficient than calling the solver an extra step and avoid
    problems because lemmas gets added.

--- src/solvers/refinement/string_refinement.cpp
+++ src/solvers/refinement/string_refinement.cpp
@@ -371,14 +371,53 @@ void string_refinementt::concretize_string(const exprt &expr)
         concretize_limit=concretize_limit>generator.max_string_length?
               generator.max_string_length:concretize_limit;
         exprt content_expr=str.content();
+
+        std::vector<exprt> result;
+        if(index_set[str.content()].empty())
+          return;
+
+        // Use the last index as the default character value
+        exprt last_concretized=simplify_expr(
+           get(str[minus_exprt(length,from_integer(1, length.type()))]), ns);
+        result.resize(concretize_limit, last_concretized);
+
+        // Keep track of the indexes for which we have actual values
+        std::set<mp_integer> initialized;
+
+        for(const auto &i : index_set[str.content()])
+        {
+          mp_integer mp_index;
+          exprt simple_i=simplify_expr(get(i), ns);
+          if(to_integer(simple_i, mp_index) ||
+             mp_index<0 ||
+             mp_index>=concretize_limit)
+            debug() << "concretize_string: ignoring out of bound index: "
+                    << from_expr(simple_i) << eom;
+          else
+          {
+            // Add an entry in the result vector
+            size_t index=mp_index.to_long();
+            exprt str_i=simplify_expr(str[simple_i], ns);
+            exprt value=simplify_expr(get(str_i), ns);
+            result[index]=value;
+            initialized.insert(index);
+          }
+        }
+
         for(size_t i=0; i<concretize_limit; ++i)
         {
-          auto i_expr=from_integer(i, str.length().type());
-          debug() << "Concretizing " << from_expr(content_expr)
-                  << " / " << i << eom;
-          current_index_set[str.content()].insert(i_expr);
-          index_set[str.content()].insert(i_expr);
+          size_t j=concretize_limit-i-1;
+          if(initialized.find(j)!=initialized.end())
+            last_concretized=result[j];
+          else
+            result[j]=last_concretized;
         }
+
+        array_exprt arr(to_array_type(content.type()));
+        arr.operands()=result;
+        debug() << "Concretized " << from_expr(content_expr)
+                << " = " << from_expr(arr) << eom;
+        found_content[content]=arr;
       }
     }
   }
@@ -574,6 +613,9 @@ decision_proceduret::resultt string_refinementt::dec_solve()
     }
   }
 
+  found_length.clear();
+  found_content.clear();
+
   initial_index_set(universal_axioms);
   update_index_set(cur);
   cur.clear();
@@ -613,7 +655,7 @@ decision_proceduret::resultt string_refinementt::dec_solve()
         if(do_concretizing)
         {
           concretize_results();
-          do_concretizing=false;
+          return D_SATISFIABLE;
         }
         else
         {
@@ -1803,6 +1845,10 @@ exprt string_refinementt::get(const exprt &expr) const
   replace_expr(symbol_resolve, ecopy);
   if(is_char_array(ecopy.type()))
   {
+    auto it_content=found_content.find(ecopy);
+    if(it_content!=found_content.end())
+     return it_content->second;
+
     auto it=found_length.find(ecopy);
     if(it!=found_length.end())
       return get_array(ecopy, it->second);
