2775e6f35122b5ff0e9b0a1c6f3ecc828766fe7c
Romain Brenguier <romain.brenguier@gmail.com>
2017-04-21 14:51:49 +0100

    Deactivating the old preprocessing

--- src/cbmc/cbmc_parse_options.cpp
+++ src/cbmc/cbmc_parse_options.cpp
@@ -911,8 +911,10 @@ bool cbmc_parse_optionst::process_goto_program(
     if(cmdline.isset("refine-strings"))
     {
       status() << "Preprocessing for string refinement" << eom;
+#if 0
       string_refine_preprocesst(
         symbol_table, goto_functions, ui_message_handler);
+#endif
     }
 
     // remove returns, gcc vectors, complex
