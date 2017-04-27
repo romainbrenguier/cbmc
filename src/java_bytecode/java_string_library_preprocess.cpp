7e6c2966fc250535c41a3a68c452bfd77019b3b2
Romain Brenguier <romain.brenguier@gmail.com>
2017-04-27 11:35:43 +0100

    [preprocessing] Float to string conversion in scientific notation

--- src/java_bytecode/java_string_library_preprocess.cpp
+++ src/java_bytecode/java_string_library_preprocess.cpp
@@ -1014,6 +1014,7 @@ Function: get_exponent
     spec - specification for floating points
 
  Outputs:
+    a java integer representing the exponent
 
  Purpose: Gets the unbiased exponent in a floating-point bit-vector
 
@@ -1023,11 +1024,13 @@ exprt get_exponent(
   const exprt &src,
   const ieee_float_spect &spec)
 {
-  exprt res_plus_128=extractbits_exprt(
+  exprt exp_bits=extractbits_exprt(
     src, spec.f+spec.e-1, spec.f,
     unsignedbv_typet(spec.e));
-  return minus_exprt(typecast_exprt(res_plus_128, java_int_type()),
-                     from_integer(128, java_int_type()));
+  // exponent is in biased from (numbers form -128 to 127 are encoded with
+  // integer from 0 to 255) we have to remove the bias.
+  return minus_exprt(typecast_exprt(exp_bits, java_int_type()),
+                     from_integer(spec.bias(), java_int_type()));
 }
 
 /*******************************************************************\
@@ -1040,7 +1043,7 @@ Function: get_magnitude
 
  Outputs:
 
- Purpose: Gets the magnitude
+ Purpose: Gets the magnitude without hidden bit
 
 \*******************************************************************/
 
@@ -1049,8 +1052,34 @@ exprt get_magnitude(
   const ieee_float_spect &spec)
 {
   return extractbits_exprt(
-    src, spec.f, 0,
+    src, spec.f-1, 0,
     unsignedbv_typet(spec.f));
+}
+
+/*******************************************************************\
+
+Function: get_significand
+
+  Inputs:
+    src - a floating point expression
+    spec - specification for floating points
+
+ Outputs:
+
+ Purpose: Gets the significand as a java integer, looking for the hidden bit
+
+\*******************************************************************/
+
+exprt get_significand(
+  const exprt &src,
+  const ieee_float_spect &spec)
+{
+  typecast_exprt magnitude(get_magnitude(src, spec), java_int_type());
+  exprt exponent=get_exponent(src, spec);
+  equal_exprt all_zeros(exponent, from_integer(0, exponent.type()));
+  plus_exprt with_hidden_bit(
+    magnitude, from_integer(0x800000, java_int_type()));
+  return if_exprt(all_zeros, magnitude, with_hidden_bit);
 }
 
 /*******************************************************************\
@@ -1073,7 +1102,14 @@ exprt single_precision_float(float f)
 Function:  estimate_decimal_exponent
 
 Purpose:
-         log10(d2) ~=~  log10(2) * bin_exp
+         We are looking for d such that n * 10^d = m * 10^e, so:
+         d = log_10(m) + log_10(2) * e - log_10(n)
+         m - the magnitude - should be between 1 and 2 so log_10(m)
+         in [0,log_10(2)]
+         n - the magnitude in base 10 - should be between 1 and 10 so
+         log_10(n) in [0, 1]
+         So the estimate is given by:
+         d ~=~  floor( log10(2) * e)
 
 \*******************************************************************/
 
@@ -1082,9 +1118,9 @@ exprt log_10_of_2=single_precision_float(0.301029995663981);
 exprt estimate_decimal_exponent(const exprt &f,  const ieee_float_spect &spec)
 {
   exprt bin_exp=get_exponent(f, spec);
-  return plus_exprt(
-    single_precision_float(1e0),
-    mult_exprt(typecast_exprt(bin_exp, java_float_type()), log_10_of_2));
+  mult_exprt dec_exponent(
+    typecast_exprt(bin_exp, java_float_type()), log_10_of_2);
+  return typecast_exprt(dec_exponent, java_int_type());
 }
 
 
@@ -1129,22 +1165,36 @@ exprt get_first_character_from_log_representation(const exprt &log)
   return ret;
 }
 
+// Table for values of 2^e / 10^(floor(log_10(2) * e)) for e from 0 to 128
+std::vector<double> two_power_e_over_ten_power_d_table(
+{1, 2, 4, 8, 1.6, 3.2, 6.4, 1.28, 2.56, 5.12, 1.024,
+ 2.048, 4.096, 8.192, 1.6384, 3.2768, 6.5536, 1.31072, 2.62144, 5.24288, 1.04858,
+ 2.09715, 4.1943, 8.38861, 1.67772, 3.35544, 6.71089, 1.34218, 2.68435, 5.36871, 1.07374,
+ 2.14748, 4.29497, 8.58993, 1.71799, 3.43597, 6.87195, 1.37439, 2.74878, 5.49756, 1.09951,
+ 2.19902, 4.39805, 8.79609, 1.75922, 3.51844, 7.03687, 1.40737, 2.81475, 5.6295, 1.1259,
+ 2.2518, 4.5036, 9.0072, 1.80144, 3.60288, 7.20576, 1.44115, 2.8823, 5.76461, 1.15292,
+ 2.30584, 4.61169, 9.22337, 1.84467, 3.68935, 7.3787, 1.47574, 2.95148, 5.90296, 1.18059,
+ 2.36118, 4.72237, 9.44473, 1.88895, 3.77789, 7.55579, 1.51116, 3.02231, 6.04463, 1.20893,
+ 2.41785, 4.8357, 9.67141, 1.93428, 3.86856, 7.73713, 1.54743, 3.09485, 6.1897, 1.23794,
+ 2.47588, 4.95176, 9.90352, 1.9807, 3.96141, 7.92282, 1.58456, 3.16913, 6.33825, 1.26765,
+ 2.5353, 5.0706, 1.01412, 2.02824, 4.05648, 8.11296, 1.62259, 3.24519, 6.49037, 1.29807,
+ 2.59615, 5.1923, 1.03846, 2.07692, 4.15384, 8.30767, 1.66153, 3.32307, 6.64614, 1.32923,
+ 2.65846, 5.31691, 1.06338, 2.12676, 4.25353, 8.50706, 1.70141});
+
 
 /*******************************************************************\
 
-Function:  get_first_character_from_log_representation
+Function: java_string_library_preprocesst::code_for_scientific_notation
 
- A float is represented as f=m*2^e where
- 0 <= m < 2 is the magnitude and -128 < e <= 128 is the exponent
- We want an alternate representation by finding n and d
- such that f=n*10^d
- We can estimate d using the following:
- d ~= log_10(f/n) ~= log_10(m) + log_10(2) * e - log_10(n)
- Then n can be expressed by the equation:
- log_10(n) = log_10(m) + log_10(2) * e - d
- log_10(m) can be 0 or -1
- We use the function get_first_character_from_log_representation
- to write n from its log.
+Purpose:
+         A float is represented as $f=m*2^e$ where
+         $0 <= m < 2$ is the significand and $-126 <= e <= 127$ is the exponent
+         We want an alternate representation by finding n and d
+         such that $f=n*10^d$. We can estimate d using the following:
+         $d ~= log_10(f/n) ~= log_10(m) + log_10(2) * e - log_10(n)$
+         Then $n$ can be expressed by the equation:
+         $log_10(n) = log_10(m) + log_10(2) * e - d$
+         log_10(m) can be 0 or -1
 
 \*******************************************************************/
 
@@ -1159,16 +1209,111 @@ codet java_string_library_preprocesst::code_for_scientific_notation(
 {
   code_blockt code;
 
+  // `binary_exponent` is $e$ in the formulas
   exprt binary_exponent=get_exponent(arg, float_spec);
-  typecast_exprt decimal_exponent(
-    estimate_decimal_exponent(arg, float_spec), java_int_type());
 
-  exprt log_n=minus_exprt(
-    mult_exprt(typecast_exprt(binary_exponent, java_float_type()), log_10_of_2),
-    typecast_exprt(decimal_exponent, java_float_type()));
+  // `binary_significand` is `m` in the formulas
+  exprt binary_significand=get_significand(arg, float_spec);
 
-  string_exprt exponent_string=fresh_string_expr(
+  // `decimal_exponent` is $d$ in the formulas
+  exprt decimal_exponent=estimate_decimal_exponent(arg, float_spec);
+
+  // bias_factor is $2^e/10^d$
+  array_exprt bias_table(
+    array_typet(java_float_type(), from_integer(128, java_int_type())));
+  for(const auto &f:two_power_e_over_ten_power_d_table)
+    bias_table.copy_to_operands(single_precision_float(f));
+  index_exprt bias_factor(bias_table, binary_exponent, java_float_type());
+
+  // `dec_significand` is $n = m * bias_factor$
+  mult_exprt dec_significand(
+    typecast_exprt(binary_significand, java_float_type()), bias_factor);
+
+  // we divide this number by 0x80000 because it represents a fraction
+  // and multiply by 1000000 to get more digits
+  mult_exprt dec_significand_with_8_digits(
+    dec_significand, single_precision_float(1.192092896));
+  typecast_exprt dec_significand_int(
+    dec_significand_with_8_digits,
+    java_int_type());
+
+  // The first character is given by dec_significand_int / 1000000
+  div_exprt dec_significand_integer_part(
+    dec_significand_int, from_integer(1000000, java_int_type()));
+  string_exprt string_first_character=fresh_string_expr(
+     refined_string_type, loc, symbol_table);
+  code.copy_to_operands(
+    code_assign_function_to_string_expr(
+      string_first_character,
+      ID_cprover_string_of_int_func,
+      {dec_significand_integer_part},
+      symbol_table));
+
+  // string_lit_dot = "."
+  string_exprt string_lit_dot=fresh_string_expr(
     refined_string_type, loc, symbol_table);
+
+  code.copy_to_operands(
+    code_assign_string_literal_to_string_expr(
+      string_lit_dot, tmp_string, ".", symbol_table));
+
+  // string_expr_with_dot = concat(string_magnitude, string_lit_dot)
+  string_exprt string_expr_with_dot=fresh_string_expr(
+     refined_string_type, loc, symbol_table);
+  code.copy_to_operands(
+    code_assign_function_to_string_expr(
+      string_expr_with_dot,
+      ID_cprover_string_concat_func,
+      {string_first_character, string_lit_dot},
+      symbol_table));
+
+  // string_fractional_part
+  minus_exprt dec_significand_fractional_part(
+    dec_significand_int,
+    mult_exprt(dec_significand_integer_part,
+               from_integer(1000000, java_int_type())));
+
+  string_exprt string_fractional_part=fresh_string_expr(
+     refined_string_type, loc, symbol_table);
+  code.copy_to_operands(
+    code_assign_function_to_string_expr(
+      string_fractional_part,
+      ID_cprover_string_of_int_func,
+      {typecast_exprt(dec_significand_fractional_part, java_int_type())},
+      symbol_table));
+
+  // string_expr_with_fractional_part =
+  //   concat(string_with_do, string_fractional_part)
+  string_exprt string_expr_with_fractional_part=fresh_string_expr(
+     refined_string_type, loc, symbol_table);
+  code.copy_to_operands(
+    code_assign_function_to_string_expr(
+      string_expr_with_fractional_part,
+      ID_cprover_string_concat_func,
+      {string_expr_with_dot, string_fractional_part},
+      symbol_table));
+
+  // string_lit_E = "E"
+  string_exprt string_lit_E=fresh_string_expr(
+    refined_string_type, loc, symbol_table);
+
+  code.copy_to_operands(
+    code_assign_string_literal_to_string_expr(
+      string_lit_E, tmp_string, "E", symbol_table));
+
+  // string_expr_with_E = concat(string_magnitude, string_lit_E)
+  string_exprt string_expr_with_E=fresh_string_expr(
+     refined_string_type, loc, symbol_table);
+  code.copy_to_operands(
+    code_assign_function_to_string_expr(
+      string_expr_with_E,
+      ID_cprover_string_concat_func,
+      {string_expr_with_fractional_part, string_lit_E},
+      symbol_table));
+
+  // exponent_string = string_of_int(decimal_exponent)
+  string_exprt exponent_string=fresh_string_expr(
+        refined_string_type, loc, symbol_table);
 
   code.copy_to_operands(
     code_assign_function_to_string_expr(
@@ -1177,40 +1322,12 @@ codet java_string_library_preprocesst::code_for_scientific_notation(
       {decimal_exponent},
       symbol_table));
 
-  string_exprt string_magnitude=fresh_string_expr(
-     refined_string_type, loc, symbol_table);
-  exprt first_character=get_first_character_from_log_representation(log_n);
-  code.copy_to_operands(
-    code_assign_function_to_string_expr(
-      string_magnitude,
-      ID_cprover_string_of_char_func,
-      {first_character},
-      symbol_table));
-
-  // string_lit_E = "E"
-  string_exprt string_lit_E=fresh_string_expr(
-     refined_string_type, loc, symbol_table);
-
-  code.copy_to_operands(
-    code_assign_string_literal_to_string_expr(
-      string_lit_E, tmp_string, "E", symbol_table));
-
-  // string_expr1 = concat(string_magnitude, string_lit_E)
-  string_exprt string_expr1=fresh_string_expr(
-     refined_string_type, loc, symbol_table);
-  code.copy_to_operands(
-    code_assign_function_to_string_expr(
-      string_expr1,
-      ID_cprover_string_concat_func,
-      {string_magnitude, string_lit_E},
-      symbol_table));
-
-  // string_expr = concat(string_expr1, exponent_string)
+  // string_expr = concat(string_expr_withE, exponent_string)
   code.copy_to_operands(
     code_assign_function_to_string_expr(
       string_expr,
       ID_cprover_string_concat_func,
-      {string_expr1, exponent_string},
+      {string_expr_with_E, exponent_string},
       symbol_table));
 
   return code;
@@ -1371,7 +1488,7 @@ codet java_string_library_preprocesst::make_float_to_string_code(
       {with_dot_string_expr, fractional_part_string_expr},
       symbol_table));
 
-//  case_list.push_back(case_simple_notation);
+  case_list.push_back(case_simple_notation);
 
   // Combining all cases
   for(std::size_t i=1; i<case_list.size(); i++)
