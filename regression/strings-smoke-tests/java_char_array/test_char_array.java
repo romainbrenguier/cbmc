296baf7230472c05b5e20b33ab409e456b77ff01
Romain Brenguier <romain.brenguier@gmail.com>
2017-09-05 14:56:36 +0100

    [regression] including model in test or append char
    [regression] Including model in test for string to char array
    
    [regression] Using model of char array functions in tests
    
    Also correct java_if test description file
    
    [regression] Remove java_object_allocation test
    
    This should be replaced by unit tests.
    
    Removing java_append_char test duplicate of the one in string-smoke-test
    
    [regression] Adding model of toCharArray inside a regression test
    
    [regression] Including models of char array functions for some string
    regression tests

--- regression/strings-smoke-tests/java_char_array/test_char_array.java
+++ regression/strings-smoke-tests/java_char_array/test_char_array.java
@@ -1,9 +1,23 @@
+
+
 public class test_char_array
 {
+  // This is a model of the String.toCharArray method
+  public static char[] toCharArray(String s)
+    {
+      int length=s.length();
+      assert(length<5);
+      char arr[]=new char[s.length()];
+      // We limit arbitrarly the loop unfolding to 5
+      for(int i=0; i<length && i<5; i++)
+	arr[i]=s.charAt(i);
+      return arr;
+    }
+  
    public static void main(int i)
    {
       String s = "abc";
-      char [] str = s.toCharArray();
+      char [] str = toCharArray(s);
       char c = str[2];
       char a = s.charAt(0);
       assert(str.length == 3);
