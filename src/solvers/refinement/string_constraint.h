7df2df1f05ed1ee6477a1624d5b23a576122e6a3
Lukasz A.J. Wrona <lukasz.andrzej.wrona@gmail.com>
2017-09-27 11:47:30 +0100

    Move string_constraint function definitions to cpp file

--- src/solvers/refinement/string_constraint.h
+++ src/solvers/refinement/string_constraint.h
@@ -77,29 +77,17 @@ replace(string_constraintt &axiom, const union_find_replacet &symbol_resolve)
   symbol_resolve.replace_expr(axiom.lower_bound);
 }
 
-inline exprt univ_within_bounds(const string_constraintt &axiom)
-{
-  return and_exprt(
-    binary_relation_exprt(axiom.lower_bound, ID_le, axiom.univ_var),
-    binary_relation_exprt(axiom.upper_bound, ID_gt, axiom.univ_var));
-}
+exprt univ_within_bounds(const string_constraintt &axiom);
 
 /// Used for debug printing.
 /// \param [in] ns: namespace for `from_expr`
 /// \param [in] identifier: identifier for `from_expr`
 /// \param [in] expr: constraint to render
 /// \return rendered string
-inline std::string from_expr(
+std::string from_expr(
   const namespacet &ns,
   const irep_idt &identifier,
-  const string_constraintt &expr)
-{
-  return "forall " + from_expr(ns, identifier, expr.univ_var) + " in [" +
-         from_expr(ns, identifier, expr.lower_bound) + ", " +
-         from_expr(ns, identifier, expr.upper_bound) + "). " +
-         from_expr(ns, identifier, expr.premise) + " => " +
-         from_expr(ns, identifier, expr.body);
-}
+  const string_constraintt &expr);
 
 /// Constraints to encode non containement of strings.
 class string_not_contains_constraintt final : public exprt
@@ -164,41 +152,14 @@ public:
 /// \param [in] identifier: identifier for `from_expr`
 /// \param [in] expr: constraint to render
 /// \return rendered string
-inline std::string from_expr(
+std::string from_expr(
   const namespacet &ns,
   const irep_idt &identifier,
-  const string_not_contains_constraintt &expr)
-{
-  return "forall x in ["+
-    from_expr(ns, identifier, expr.univ_lower_bound())+", "+
-    from_expr(ns, identifier, expr.univ_upper_bound())+"). "+
-    from_expr(ns, identifier, expr.premise())+" => ("+
-    "exists y in ["+from_expr(ns, identifier, expr.exists_lower_bound())+", "+
-    from_expr(ns, identifier, expr.exists_upper_bound())+"). "+
-    from_expr(ns, identifier, expr.s0())+"[x+y] != "+
-    from_expr(ns, identifier, expr.s1())+"[y])";
-}
+  const string_not_contains_constraintt &expr);
 
-inline const string_not_contains_constraintt
-&to_string_not_contains_constraint(const exprt &expr)
-{
-  PRECONDITION(expr.id()==ID_string_not_contains_constraint);
-  DATA_INVARIANT(
-    expr.operands().size()==7,
-    string_refinement_invariantt("string_not_contains_constraintt must have 7 "
-      "operands"));
-  return static_cast<const string_not_contains_constraintt &>(expr);
-}
+const string_not_contains_constraintt &
+to_string_not_contains_constraint(const exprt &expr);
 
-inline string_not_contains_constraintt
-&to_string_not_contains_constraint(exprt &expr)
-{
-  PRECONDITION(expr.id()==ID_string_not_contains_constraint);
-  DATA_INVARIANT(
-    expr.operands().size()==7,
-    string_refinement_invariantt("string_not_contains_constraintt must have 7 "
-      "operands"));
-  return static_cast<string_not_contains_constraintt &>(expr);
-}
+string_not_contains_constraintt &to_string_not_contains_constraint(exprt &expr);
 
 #endif
