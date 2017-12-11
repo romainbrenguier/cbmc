45919ca573af48eec7ca0379e9ead08412affd89
Romain Brenguier <romain.brenguier@gmail.com>
2017-12-11 16:37:04 +0000

    Class for reg-expr generating solver constraints

---
+++ src/solvers/refinement/regular_expression.cpp
@@ -0,0 +1,120 @@
+/*******************************************************************\
+
+Module: Generates string constraints for regular expressions.
+
+Author: Romain Brenguier, romain.brenguier@diffblue.com
+
+\*******************************************************************/
+
+#include "regular_expression.h"
+
+std::vector<std::pair<char, char>> charsett::parse(const std::string &s)
+{
+  std::vector<std::pair<char, char>> intervals;
+  const std::size_t length = s.length();
+  for(std::size_t i=0; i < length; ++i)
+  {
+    if(i+2 < length && s[i+1] == '-')
+    {
+      if(s[i] < s[i+2])
+        intervals.emplace_back(s[i], s[i+2]);
+      else
+        intervals.emplace_back(s[i+2], s[i]);
+      i = i + 2;
+    }
+    else
+      intervals.emplace_back(s[i], s[i]);
+  }
+  return intervals;
+}
+
+/// Return an expression which is true when the expression is contained in the
+/// charset
+exprt charsett::contains(const exprt &char_expr) const
+{
+  std::vector<exprt> ops;
+  for(const auto &pair : intervals)
+  {
+    const exprt min = from_integer(pair.first, char_expr.type());
+    const exprt max = from_integer(pair.second, char_expr.type());
+    ops.push_back(and_exprt(
+      binary_relation_exprt(min, ID_le, char_expr),
+      binary_relation_exprt(char_expr, ID_le, max)));
+  }
+  return disjunction(ops);
+}
+
+/// Compiles a flat pattern.
+/// The only special characters are '[', '?', '*' and '+'.
+/// '(', '|' and such are considered as the corresponding characters and don't
+/// have a special meaning.
+std::vector<atomic_patternt> flat_patternt::parse(const std::string &regex)
+{
+  std::vector<atomic_patternt> result;
+  const std::size_t length = regex.length();
+  for(std::size_t i = 0; i < length;) // `i` is incremented inside the loop
+  {
+    const std::size_t start_index = regex[i] == '[' ? i + 1 : i;
+    std::size_t end_index = regex[i] == '[' ? regex.find(']', i) : i + 1;
+    const charsett cs(regex.substr(start_index, end_index));
+
+    quantifiert quant = quantifiert::ONCE;
+    if(end_index + 1 < length)
+    {
+      if(regex[end_index + 1] == '?')
+      {
+        quant = quantifiert::QUESTION_MARK;
+        end_index += 1;
+      }
+      else if(regex[end_index + 1] == '+')
+      {
+        quant = quantifiert::PLUS;
+        end_index += 1;
+      }
+      else if(regex[end_index + 1] == '*')
+      {
+        quant = quantifiert::STAR;
+        end_index += 1;
+      }
+    }
+
+    result.emplace_back(cs, quant);
+    i = end_index;
+  }
+  return result;
+}
+
+/// \param usable_name: a name we can use as a prefix for symbols without
+///        interfering without other symbols.
+std::vector<string_constraintt> flat_patternt::generate_constraints(
+  const symbol_exprt &match_result,
+  const refined_string_exprt &str,
+  const std::string &usable_name) const
+{
+  std::vector<string_constraintt> result;
+  const typet length_type = str.length().type();
+  symbol_exprt q_var(usable_name+"_QA", length_type);
+  std::vector<symbol_exprt> delimiters;
+  for(std::size_t i = 0; i < atoms.size(); ++i)
+  {
+    delimiters.emplace_back(
+      usable_name + "_" + std::to_string(i), length_type);
+  }
+
+  for(std::size_t i = 0; i < atoms.size(); ++i)
+  {
+    // forall q_var \in [d[i-1], d[i]]. str[i] \in charset
+    const exprt lower_bound =
+      i == 0
+      ? static_cast<exprt>(from_integer(0, length_type))
+      : delimiters[i-1];
+
+    string_constraintt sc(
+      q_var,
+      lower_bound,
+      delimiters[i],
+      match_result,
+      atoms[i].charset.contains(str[q_var]));
+  }
+  return result;
+}
---
+++ src/solvers/refinement/regular_expression.h
@@ -0,0 +1,65 @@
+/*******************************************************************\
+
+Module: Generates string constraints for regular expressions.
+
+Author: Romain Brenguier, romain.brenguier@diffblue.com
+
+\*******************************************************************/
+
+#ifndef CPROVER_SOLVERS_REFINEMENT_REGULAR_EXPRESSION_H
+#define CPROVER_SOLVERS_REFINEMENT_REGULAR_EXPRESSION_H
+
+#include <string>
+#include <vector>
+#include "string_constraint.h"
+
+class charsett
+{
+public:
+  explicit charsett(const std::string &s) : intervals(parse(s)) { };
+  explicit charsett(const char c) : charsett(std::string(c, 1)) { };
+
+  exprt contains(const exprt &char_expr) const;
+
+private:
+  // The charset is represented by a list of intervals.
+  // We use mp_integer because char may not be limited to 8 bits.
+  const std::vector<std::pair<char, char>> intervals;
+
+  static std::vector<std::pair<char, char>> parse(const std::string &str);
+};
+
+enum class quantifiert { QUESTION_MARK, STAR, PLUS, ONCE};
+
+/// What we call atomic patternt are regular expressions of the form
+/// [charset]* or [charset]+ or [charset]? or [charset]
+class atomic_patternt
+{
+public:
+  atomic_patternt(charsett charset, quantifiert quantifier=quantifiert::ONCE)
+    :charset(charset), quantifier(quantifier)
+  { };
+
+  const charsett charset;
+  const quantifiert quantifier;
+};
+
+/// Regular expression with no nested quantifiers.
+/// For instance (a b)* is flat but (a* b)* is not.
+class flat_patternt
+{
+public:
+  explicit flat_patternt(const std::string &regex): atoms(parse(regex))
+  { };
+
+  std::vector<string_constraintt> generate_constraints(
+    const symbol_exprt &match_result,
+    const refined_string_exprt &str,
+    const std::string &usable_name) const;
+
+private:
+  static std::vector<atomic_patternt> parse(const std::string &regex);
+  const std::vector<atomic_patternt> atoms;
+};
+
+#endif // CPROVER_SOLVERS_REFINEMENT_REGULAR_EXPRESSION_H
