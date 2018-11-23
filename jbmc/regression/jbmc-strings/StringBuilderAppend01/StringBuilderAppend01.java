b67ac3c44215f264445082f873434d760d0aab28
Romain Brenguier <romain.brenguier@gmail.com>
2018-11-23 14:02:56 +0000

    Debug tests

--- jbmc/regression/jbmc-strings/StringBuilderAppend01/StringBuilderAppend01.java
+++ jbmc/regression/jbmc-strings/StringBuilderAppend01/StringBuilderAppend01.java
@@ -15,27 +15,29 @@ public class StringBuilderAppend01
       StringBuilder lastBuffer = new StringBuilder("last buffer");
       StringBuilder buffer = new StringBuilder();
 
-      buffer.append(objectRef)
-            .append("%n")
-            .append(string)
-            .append("%n")
-            .append(charArray)
-            .append("%n")
-            .append(charArray, 0, 3)
-            .append("%n")
-            .append(booleanValue)
-            .append("%n")
-            .append(characterValue)
-            .append("%n")
-            .append(integerValue)
-            .append("%n")
-            .append(longValue)
-            .append("%n")
-            .append(floatValue)
-            .append("%n")
-            .append(doubleValue)
-            .append("%n")
-            .append(lastBuffer);
+      buffer
+        // .append(objectRef)
+        //           .append("%n")
+        //    .append(string)
+        //   .append("%n")
+        //     .append(charArray)
+        //  .append("%n")
+          .append(charArray, 0, 3)
+        // .append("%n")
+        //    .append(booleanValue)
+        // .append("%n")
+          .append(characterValue)
+        //    .append("%n")
+      // .append(integerValue)
+      //.append("%n");
+        //    .append(longValue)
+        //    .append("%n")
+//            .append(floatValue)
+        //          .append("%n")
+        //  .append(doubleValue)
+        //  .append("%n")
+//    .append(lastBuffer);
+        ;
 
       String tmp=buffer.toString();
       assert tmp.equals("diffblue%ntest%nverification%nver%ntrue%nZ%n7%n10000000000%n2.5%n33.333%nlast buffer");
