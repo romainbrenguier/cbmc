e6b53d7cbdf9e7da29159147d7e362e47330f07c
Chris Smowton <chris@smowton.net>
2018-12-18 10:47:18 +0000

    Use `..` and `[[]]` for SSA expression field separator
    This makes it easier to read the difference between an atomic symbol and a field
    of a composite.

--- src/util/ssa_expr.cpp
+++ src/util/ssa_expr.cpp
@@ -31,8 +31,8 @@ static void build_ssa_identifier_rec(
 
     build_ssa_identifier_rec(member.struct_op(), l0, l1, l2, os, l1_object_os);
 
-    os << '.' << member.get_component_name();
-    l1_object_os << '.' << member.get_component_name();
+    os << ".." << member.get_component_name();
+    l1_object_os << ".." << member.get_component_name();
   }
   else if(expr.id()==ID_index)
   {
@@ -41,8 +41,8 @@ static void build_ssa_identifier_rec(
     build_ssa_identifier_rec(index.array(), l0, l1, l2, os, l1_object_os);
 
     const mp_integer idx = numeric_cast_v<mp_integer>(index.index());
-    os << '[' << idx << ']';
-    l1_object_os << '[' << idx << ']';
+    os << "[[" << idx << "]]";
+    l1_object_os << "[[" << idx << "]]";
   }
   else if(expr.id()==ID_symbol)
   {
