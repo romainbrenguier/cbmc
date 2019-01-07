5a09d62de6953d7729d6efc087fc3ebb63eaabb7
Romain Brenguier <romain.brenguier@gmail.com>
2019-01-07 11:25:59 +0000

    Profiling with backtrace

--- src/util/profiler.h
+++ src/util/profiler.h
@@ -7,14 +7,55 @@
 
 #include <chrono>
 #include <iostream>
+#include <memory>
+#include <execinfo.h>
+#include <cxxabi.h>
+#include "freer.h"
 
-#define NO_PROFILING
+// #define NO_PROFILING
 
 #ifdef NO_PROFILING
 #define PROFILER_BREAKPOINT
 #else
 #define PROFILER_BREAKPOINT profiler_breakpoint(__FILE__, __LINE__)
 #endif
+
+#define BACKTRACE_SIZE 1
+
+inline std::string get_backtrace_string()
+{
+  std::ostringstream out;
+  void * stack[BACKTRACE_SIZE + 2] = {};
+
+  std::size_t entries=backtrace(stack, sizeof(stack) / sizeof(void *));
+  std::unique_ptr<char*, freert> description(
+    backtrace_symbols(stack, entries));
+
+  for(std::size_t i=2; i<entries; i++)
+  {
+    out << '<' << std::flush;
+    std::string working = description.get()[i];
+    std::string::size_type start=working.rfind('(');  // Path may contain '(' !
+    std::string::size_type plus_pos=working.find('+', start);
+    if(start != std::string::npos && plus_pos != std::string::npos)
+    {
+
+      std::string::size_type length=plus_pos-(start+1);
+      std::string mangled(working.substr(start+1, length));
+      int demangle_success=1;
+      std::unique_ptr<char, freert> demangled(
+        abi::__cxa_demangle(
+          mangled.c_str(), nullptr, nullptr, &demangle_success));
+      if(demangle_success==0)
+      {
+        std::string function = demangled.get();
+        std::string::size_type name_end = function.find('(');
+        out << function.substr(0, name_end);
+      }
+    }
+  }
+  return out.str();
+}
 
 static std::chrono::time_point<std::chrono::steady_clock>
   profiler_last_time_point = std::chrono::steady_clock::now();
@@ -23,11 +64,17 @@ inline void profiler_breakpoint(const std::string &file, unsigned line)
 {
   auto now = std::chrono::steady_clock::now();
   auto diff = std::chrono::duration<double>(now - profiler_last_time_point);
-  if(file.length() < 30)
-    std::cout << file;
+  auto start = file.find_last_of('/');
+
+  std::string id =
+    start != std::string::npos
+    ? file.substr(start+1) + ":" + std::to_string(line) + get_backtrace_string()
+    : file + ":" + std::to_string(line) + get_backtrace_string();
+  if(id.length() < 120)
+    std::cout << id;
   else
-    std::cout << "..." << file.substr(file.length() - 27);
-  std::cout << ":" << line << " " << diff.count() << std::endl;
+    std::cout << id.substr(0, 117) << "...";
+  std::cout << " " << diff.count() << std::endl;
   profiler_last_time_point = std::chrono::steady_clock::now();
 }
 
