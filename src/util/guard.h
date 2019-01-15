44130202bcc3a8cc2ae9dd467b7cfc4bc18292f9
Romain Brenguier <romain.brenguier@gmail.com>
2019-01-15 14:54:33 +0000

    Switch between guard bdd and guard expr
    Using a HAS_CUDD flag

--- src/util/guard.h
+++ src/util/guard.h
@@ -12,9 +12,20 @@ Author: Daniel Kroening, kroening@kroening.com
 #ifndef CPROVER_UTIL_GUARD_H
 #define CPROVER_UTIL_GUARD_H
 
+#ifdef HAS_CUDD
+
+#include "guard_bdd.h"
+
+using guard_managert = guard_bdd_managert;
+using guardt = guard_bddt;
+
+#else
+
 #include "guard_expr.h"
 
 using guard_managert = guard_expr_managert;
 using guardt = guard_exprt;
+
+#endif // HAS_CUDD
 
 #endif // CPROVER_UTIL_GUARD_H
