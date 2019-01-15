61c1c830f140f4b73fd30c4deffa6aec18e1f278
Romain Brenguier <romain.brenguier@gmail.com>
2019-01-15 14:54:29 +0000

    Move guard implementation to guard_expr.h/cpp
    This is to clearly seperate the two implementations.
    The choice between the two will be made in guard.h

--- src/util/guard.h
+++ src/util/guard.h
@@ -12,62 +12,6 @@ Author: Daniel Kroening, kroening@kroening.com
 #ifndef CPROVER_UTIL_GUARD_H
 #define CPROVER_UTIL_GUARD_H
 
-#include <iosfwd>
-
-#include "std_expr.h"
-
-/// This is unused by this implementation of guards, but can be used by other
-/// implementations of the same interface.
-struct guard_managert
-{
-};
-
-class guardt
-{
-public:
-  explicit guardt(const exprt &e, guard_managert &manager)
-    : manager(manager), expr(e)
-  {
-  }
-
-  guardt &operator=(const guardt &other)
-  {
-    expr = other.expr;
-    manager = other.manager;
-    return *this;
-  }
-
-  void add(const exprt &expr);
-
-  void append(const guardt &guard)
-  {
-    add(guard.as_expr());
-  }
-
-  exprt as_expr() const
-  {
-    return expr;
-  }
-
-  void guard_expr(exprt &dest) const;
-
-  bool is_true() const
-  {
-    return expr.is_true();
-  }
-
-  bool is_false() const
-  {
-    return expr.is_false();
-  }
-
-  friend guardt &operator -= (guardt &g1, const guardt &g2);
-  friend guardt &operator |= (guardt &g1, const guardt &g2);
-
-  guard_managert &manager;
-
-private:
-  exprt expr;
-};
+#include "guard_expr.h"
 
 #endif // CPROVER_UTIL_GUARD_H
---
+++ src/util/guard_expr.h
@@ -0,0 +1,75 @@
+/*******************************************************************\
+
+Module: Guard Data Structure
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+/// \file
+/// Guard Data Structure
+
+#ifndef CPROVER_UTIL_GUARD_EXPR_H
+#define CPROVER_UTIL_GUARD_EXPR_H
+
+#include <iosfwd>
+
+#include "std_expr.h"
+
+/// This is unused by this implementation of guards, but can be used by other
+/// implementations of the same interface.
+struct guard_managert
+{
+};
+
+class guardt
+{
+public:
+  explicit guardt(const exprt &e, guard_managert &manager)
+    : manager(manager), expr(e)
+  {
+  }
+
+  guardt &operator=(const guardt &other)
+  {
+    expr = other.expr;
+    manager = other.manager;
+    return *this;
+  }
+
+  void add(const exprt &expr);
+
+  void append(const guardt &guard)
+  {
+    add(guard.as_expr());
+  }
+
+  exprt as_expr() const
+  {
+    return expr;
+  }
+
+  void guard_expr(exprt &dest) const;
+
+  bool is_true() const
+  {
+    return expr.is_true();
+  }
+
+  bool is_false() const
+  {
+    return expr.is_false();
+  }
+
+  friend guardt &operator-=(guardt &g1, const guardt &g2);
+  friend guardt &operator|=(guardt &g1, const guardt &g2);
+
+  guardt operator!() const;
+
+  guard_managert &manager;
+
+private:
+  exprt expr;
+};
+
+#endif // CPROVER_UTIL_GUARD_EXPR_H
