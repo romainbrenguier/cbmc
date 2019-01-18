6b21d3f4262232df287690dbb1d2352fd15b02b9
Romain Brenguier <romain.brenguier@gmail.com>
2019-01-18 09:46:44 +0000

    Update test for concat function

--- regression/strings/test_concat/test.c
+++ regression/strings/test_concat/test.c
@@ -1,16 +1,23 @@
 #include <assert.h>
-#include "../cprover-string-hack.h"
 
+typedef struct __attribute__((__packed__)) __CPROVER_refined_string_type // NOLINT
+  { int length; char *content; } __CPROVER_refined_string_type;
+
+extern int __CPROVER_uninterpreted_string_concat_func(
+  int result_length, char *result_data,
+  __CPROVER_refined_string_type str1, __CPROVER_refined_string_type str2);
+
+extern char __CPROVER_uninterpreted_string_char_at_func(
+  __CPROVER_refined_string_type str1, int position);
 
 int main()
 {
-  __CPROVER_string s,t,u;
-  unsigned i = __CPROVER_string_length(s);
-  t = __CPROVER_string_literal("ppo");
-  u =  __CPROVER_string_concat(s, t);
-  __CPROVER_char c = __CPROVER_char_at(u,i);
+  __CPROVER_refined_string_type s,t,u;
+  int return_code =
+    __CPROVER_uninterpreted_string_concat_func(u.length, u.content, s, t);
+  char c = __CPROVER_uninterpreted_string_char_at_func(u, 2);
 
   assert(c  == 'p');
-  assert(__CPROVER_char_at(u,2)  == 'p');
+  assert(__CPROVER_uninterpreted_string_char_at_func(u,2)  == 'p');
   return 0;
 }
