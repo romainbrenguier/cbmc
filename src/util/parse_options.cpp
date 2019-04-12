0e56317b66922663bb6ca975461dc263f4cb477f
Romain Brenguier <romain.brenguier@gmail.com>
2019-04-12 08:23:32 +0100

    Remove comment made unecessary
    Now the log field gets initialized before parse_options_baset is
    constructed so there is no danger as described in the comment.

--- src/util/parse_options.cpp
+++ src/util/parse_options.cpp
@@ -34,18 +34,6 @@ parse_options_baset::parse_options_baset(
   parse_result=cmdline.parse(argc, argv, optstring.c_str());
   message_handler = util_make_unique<ui_message_handlert>(cmdline, program);
   log = messaget{*message_handler};
-
-  // DO NOT USE log HERE!
-  //
-  // The usual pattern of use is that the application class inherits from
-  // messaget and parse_options_baset using a member variable of type
-  // message_handlert to construct the messaget part.
-  //
-  // C++'s rules of initialisation mean that the constructors for
-  // messaget and then parse_options_base run before those of message_handlert.
-  // This means that the message_handlert object is uninitialised.
-  // Using it here will likely cause a hard to debug failure somewhere in
-  // the messaget code.
 }
 
 void parse_options_baset::help()
