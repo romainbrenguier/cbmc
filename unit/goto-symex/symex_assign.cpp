0f361db1e5cf676e81641789fbd33ccb70c544b4
Romain Brenguier <romain.brenguier@gmail.com>
2019-06-19 09:26:51 +0100

    Test symex_assign with constant propagation
    Test that constant values get propagated and that this does not
    interfer with a second assignment.

--- unit/goto-symex/symex_assign.cpp
+++ unit/goto-symex/symex_assign.cpp
@@ -54,21 +54,20 @@ SCENARIO(
   dirty.populate_dirty_for_function("fun", function);
   state.dirty = &dirty;
 
+  // Initialize symbol table with an integer symbol `foo`, and a boolean g
+  symbol_tablet symbol_table;
+  namespacet ns{symbol_table};
+  const signedbv_typet int_type{32};
+  const symbol_exprt foo{"foo", int_type};
+  add_to_symbol_table(symbol_table, foo);
+  const ssa_exprt ssa_foo{foo};
+  const symbol_exprt g{"g", bool_typet{}};
+  add_to_symbol_table(symbol_table, g);
+
   GIVEN("An L1 lhs and an L2 rhs of type int, and a guard g")
   {
-    // Initialize symbol table with an integer symbol `foo`
-    symbol_tablet symbol_table;
-    namespacet ns{symbol_table};
-    const signedbv_typet int_type{32};
-    const symbol_exprt foo{"foo", int_type};
-    add_to_symbol_table(symbol_table, foo);
-    const ssa_exprt ssa_foo{foo};
-
-    const symbol_exprt g{"g", bool_typet{}};
-    add_to_symbol_table(symbol_table, g);
     exprt::operandst guard;
     guard.push_back(g);
-
     null_message_handlert log;
     optionst options;
     symex_configt symex_config{options};
@@ -118,6 +117,82 @@ SCENARIO(
         {
           REQUIRE(
             to_symbol_expr(step.original_full_lhs).get_identifier() == "foo");
+        }
+      }
+    }
+  }
+  GIVEN(
+    "A lhs and rhs of type int, an empty guard, and constant propagation "
+    "activated")
+  {
+    exprt::operandst guard;
+    null_message_handlert log;
+    optionst options;
+    symex_configt symex_config{options};
+    symex_config.constant_propagation = true;
+
+    WHEN("foo is assigned without a guard")
+    {
+      const exprt rhs1 = from_integer(5721, int_type);
+      symex_target_equationt target{log};
+      exprt full_lhs = nil_exprt{};
+      full_lhs.type() = int_type;
+      symex_assign_symbol(
+        state, ssa_foo, full_lhs, rhs1, guard,
+        symex_targett::assignment_typet::STATE,
+        ns, symex_config, target);
+      THEN("An equation with an empty guard is added to the target")
+      {
+        REQUIRE(target.SSA_steps.size() == 1);
+        SSA_stept step = target.SSA_steps.back();
+        REQUIRE(step.guard == true_exprt{});
+      }
+      THEN("The propagation map contains an entry for foo")
+      {
+        const auto propagated = state.rename(ssa_foo, ns);
+        const mp_integer value =
+          numeric_cast_v<mp_integer>(to_constant_expr(propagated.get()));
+        REQUIRE(value == 5721);
+        WHEN("foo is assigned a second time")
+        {
+          const exprt rhs2 = from_integer(1841, int_type);
+          exprt full_lhs2 = nil_exprt{};
+          full_lhs.type() = int_type;
+          symex_assign_symbol(
+            state, ssa_foo, full_lhs2, rhs2, guard,
+            symex_targett::assignment_typet::STATE,
+            ns, symex_config, target);
+          THEN("A second equation is added to the target")
+          {
+            REQUIRE(target.SSA_steps.size() == 2);
+            SSA_stept step = target.SSA_steps.back();
+            REQUIRE(step.type == goto_trace_stept::typet::ASSIGNMENT);
+            REQUIRE(step.assignment_type == symex_targett::assignment_typet::STATE);
+            REQUIRE(step.cond_expr.id() == ID_equal);
+            REQUIRE(step.cond_expr == equal_exprt{step.ssa_lhs, step.ssa_rhs});
+            REQUIRE(step.guard == true_exprt{});
+
+            THEN("The left-hand-side of the equation is foo!0#2")
+            {
+              REQUIRE(to_symbol_expr(step.ssa_lhs).get_identifier() == "foo!0#2");
+            }
+            THEN("The right-hand-side of the equation is 1841")
+            {
+              const auto rhs_value =
+                numeric_cast_v<mp_integer>(to_constant_expr(step.ssa_rhs));
+              REQUIRE(rhs_value == 1841);
+            }
+            THEN("ssa_full_lhs is foo!0#1")
+            {
+              REQUIRE(
+                to_symbol_expr(step.ssa_full_lhs).get_identifier() == "foo!0#2");
+            }
+            THEN("original_full_lhs is foo")
+            {
+              REQUIRE(
+                to_symbol_expr(step.original_full_lhs).get_identifier() == "foo");
+            }
+          }
         }
       }
     }
